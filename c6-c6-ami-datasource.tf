9c04a334f476b43b4c029447b4dbb30b3589600a
prathamdey <deypratham04@gmail.com>
2023-06-20 02:30:07 +0530

    second commit

---
+++ c6-c6-ami-datasource.tf
@@ -0,0 +1,20 @@
+# Get latest AMI ID for Amazon Linux2 OS
+data "aws_ami" "aws_ami_latest" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.0.20230517*"]
+  }
+}
